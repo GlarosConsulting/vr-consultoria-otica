d85ddf73b1f6be24a712dcd1f5609477504ea0b6
André 'Dezzy <andrevcv1@gmail.com>
2021-02-01 23:46:50 -0300

    chore: Add test chart

--- packages/mobile/src/pages/Home/index.tsx
+++ packages/mobile/src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
 
 import Header from '../../components/Header';
 
@@ -9,10 +11,44 @@ const Home: React.FC = () => (
     <Header />
 
     <Container>
-      <ContactButtonContainer activeOpacity={0.6}>
-        <ContactButtonText>Contato</ContactButtonText>
-      </ContactButtonContainer>
+      <BarChart
+        data={{
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              data: [20, 45, 28, 80, 99, 43],
+            },
+          ],
+        }}
+        width={Dimensions.get('screen').width - 32}
+        height={220}
+        yAxisLabel="$"
+        chartConfig={{
+          backgroundColor: '#777',
+          backgroundGradientFrom: '#888',
+          backgroundGradientTo: '#999',
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: '6',
+            strokeWidth: '2',
+            stroke: '#999',
+          },
+        }}
+        verticalLabelRotation={30}
+        style={{
+          borderRadius: 8,
+        }}
+      />
     </Container>
+
+    <ContactButtonContainer activeOpacity={0.6}>
+      <ContactButtonText>Contato</ContactButtonText>
+    </ContactButtonContainer>
   </>
 );
 
--- packages/mobile/src/pages/Home/styles.ts
+++ packages/mobile/src/pages/Home/styles.ts
@@ -4,6 +4,8 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+
+  padding: 16px;
 `;
 
 export const ContactButtonContainer = styled.TouchableOpacity`
